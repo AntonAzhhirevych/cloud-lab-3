3efa65c164a6c9bc6d605592e1beb20829fc3d90
Anton Azhhirevych <a.azhhirevych@lampalampa.net>
2024-11-19 13:22:50 +0200

    added a failed test

--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,7 @@
 const add = require('./calculator');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(5);
 });
 
 test('adds -1 + -2 to equal -3', () => {
